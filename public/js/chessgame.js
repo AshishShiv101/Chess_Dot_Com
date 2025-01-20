a08a1f42985e34635b9480c92f6d3dd77776e9c4
Ashish Shiv <110735303+AshishShiv101@users.noreply.github.com>
2025-01-20 14:54:59 +0530

    Logic for the chess board established

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,24 @@
 const socket = io();
+const chess = new Chess();
+const boardElement = document.querySelector('.chessboard');
 
-socket.emit("hello")
+
+let draggedPiece = null;
+let sourceSquare =  null;
+let playerRole = null;
+
+const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    board.forEach((row, rowindex)=>{
+        row.forEach((square,squareindex)=>{
+            const squareELement = document.createElement("div");
+            squareELement.classList.add("square",
+                (rowindex + squareindex)%2 ===0 ? "light" : "dark"
+            )
+        })
+    })
+
+}
+const handleMove = () => {}
+const getPiece = () => {}
